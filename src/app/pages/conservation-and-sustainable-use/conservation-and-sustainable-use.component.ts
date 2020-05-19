76f9112a4d28208abd3262730f2056ae5be91fb4
Fabrizio Antonangeli <fabrizio.antonangeli@gmail.com>
2020-05-19 17:08:27 +0200

    remove "<Filter>" from the tilte if no Country select and show the countryName

--- src/app/pages/conservation-and-sustainable-use/conservation-and-sustainable-use.component.ts
+++ src/app/pages/conservation-and-sustainable-use/conservation-and-sustainable-use.component.ts
@@ -9,13 +9,40 @@ import {BasePage01Component} from '../base-page01/base-page01.component';
   templateUrl: './conservation-and-sustainable-use.component.html',
   styleUrls: ['./conservation-and-sustainable-use.component.scss']
 })
-export class ConservationAndSustainableUseComponent extends BasePage01Component {
+export class ConservationAndSustainableUseComponent extends BasePage01Component  {
+    countryName:string;
 
     constructor(_utilsService:UtilsService){
         super(_utilsService);
     }
 
 
+
+    /**
+     * action on facet click
+     *
+     * @param {object} type the type
+     */
+    searchAggregation(event: Filter[]) {
+        if(event[0].key==="countries") this.countryName=event[0].value;
+
+        return super.searchAggregation(event);
+    }
+
+
+    /**
+     * remove a filter
+     *
+     * @param {Filter[]} filters the filters to clear
+     * @param {string} key the key
+     * @param {string} value the value
+     * @return Filter[] the filters cleared
+     */
+    removeFilterByKeyVal(filters:Filter[], key:string, value:string):Filter[]{
+        if(key==="countries") this.countryName=null;
+
+        return super.removeFilterByKeyVal(filters, key, value);
+    }
 
 
 }
