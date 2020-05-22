8f8c54d1682ce117a674daac4d23038c88ebd533
Fabrizio Antonangeli <fabrizio.antonangeli@gmail.com>
2020-05-22 17:23:42 +0200

    Continents data

---
+++ rest/continents.js
@@ -0,0 +1,30 @@
+module.exports=[
+  {
+    "name": "Asia",
+    "value": 1155
+  },
+  {
+    "name": "Europe",
+    "value": 1064
+  },
+  {
+    "name": "Africa",
+    "value": 408
+  },
+  {
+    "name": "Americas",
+    "value": 560
+  },
+  {
+    "name": "TBD",
+    "value": 129
+  },
+  {
+    "name": "Oceania",
+    "value": 171
+  },
+  {
+    "name": "",
+    "value": 31
+  }
+];
