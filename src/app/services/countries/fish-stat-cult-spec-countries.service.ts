148db2018a919394a46883ce0d5d65d2063fef23
Fabrizio Antonangeli <fabrizio.antonangeli@gmail.com>
2020-05-26 16:14:27 +0200

    Moved countries services in services/countries dir

--- src/app/services/countries/fish-stat-cult-spec-countries.service.ts
+++ src/app/services/countries/fish-stat-cult-spec-countries.service.ts
@@ -2,12 +2,12 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { shareReplay, map } from 'rxjs/operators';
-import { environment } from '../../environments/environment';
-import { LoggerService } from './logger.service';
-import {SearchServiceParams} from '../namespace';
-import {UtilsService} from './utils.service';
-import {BaseService} from './base.service';
-import { AggregationItem} from '../components/search/namespace';
+import { environment } from '../../../environments/environment';
+import { LoggerService } from '../logger.service';
+import {SearchServiceParams} from '../../namespace';
+import {UtilsService} from '../utils.service';
+import {BaseService} from '../base.service';
+import { AggregationItem} from '../../components/search/namespace';
 
 @Injectable({
   providedIn: 'root'
