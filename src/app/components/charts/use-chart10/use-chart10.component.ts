a73d4b43666702691cb3f79c32198dd2da51a776
Fabrizio Antonangeli <fabrizio.antonangeli@gmail.com>
2020-06-01 09:53:10 +0200

    render chart 10 as a barchart

--- src/app/components/charts/use-chart10/use-chart10.component.ts
+++ src/app/components/charts/use-chart10/use-chart10.component.ts
@@ -2,20 +2,20 @@ import { Component, Input, OnChanges } from '@angular/core';
 import {LoggerService} from '../../../services/logger.service';
 import {UtilsService} from '../../../services/utils.service'
 import {BaseChart01Component} from '../base-chart01/base-chart01.component';
-import {Columns01Component} from '../columns01/columns01.component';
+import {Bars01Component} from '../bars01/bars01.component';
 import {UseChart10Service} from '../../../services/use/use-chart10.service';
 import {ChartDataFormat} from '../../../namespace';
 
 @Component({
   selector: 'app-use-chart10',
   template: `
-    <app-columns01 [series]="series" *ngIf="series.length" [height]="300" xAxisTitle="No of live gene banks reported"></app-columns01>
+    <app-bars01 [series]="series" *ngIf="series.length" [height]="300" xAxisTitle="No of live gene banks reported"></app-bars01>
   `,
   styles: []
 })
 export class UseChart10Component extends BaseChart01Component implements OnChanges {
     dataFormat=ChartDataFormat.keyval;
-    
+
     constructor(_service:UseChart10Service, _utilsService:UtilsService, _logger:LoggerService) {
         super(_service, _utilsService, _logger);
     }
