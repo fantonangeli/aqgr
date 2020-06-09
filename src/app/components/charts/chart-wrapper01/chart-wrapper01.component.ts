1647590ece9b3df8411311f95e398620e930939b
Fabrizio Antonangeli <fabrizio.antonangeli@gmail.com>
2020-06-09 10:46:24 +0200

    repeat search-pills after every charts

--- src/app/components/charts/chart-wrapper01/chart-wrapper01.component.ts
+++ src/app/components/charts/chart-wrapper01/chart-wrapper01.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Filter } from '../../../components/search/namespace';
 
 @Component({
@@ -6,14 +6,17 @@ import { Filter } from '../../../components/search/namespace';
   templateUrl: './chart-wrapper01.component.html',
   styles: []
 })
-export class ChartWrapper01Component implements OnInit {
+export class ChartWrapper01Component {
 
     /** List of filters used for searching */
     @Input() filterValues: Filter[];
+    /** Event specifying the filter criteria to be deleted */
+    @Output() resetFilterCriteriaEvent: EventEmitter<Filter> = new EventEmitter<Filter>();
 
   constructor() { }
 
-  ngOnInit() {
-  }
+    removeFilter(filterCondition: Filter) {
+        this.resetFilterCriteriaEvent.emit(filterCondition);
+    }
 
 }
