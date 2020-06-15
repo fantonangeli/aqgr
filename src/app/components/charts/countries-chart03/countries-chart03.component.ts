b8b7a9774b6012e10bb28d3f77832baf531ee0cb
Fabrizio Antonangeli <fabrizio.antonangeli@gmail.com>
2020-06-15 14:39:59 +0200

    show taxonomies instead of species

--- src/app/components/charts/countries-chart03/countries-chart03.component.ts
+++ src/app/components/charts/countries-chart03/countries-chart03.component.ts
@@ -2,30 +2,25 @@ import { Component, Input, OnChanges } from '@angular/core';
 import {LoggerService} from '../../../services/logger.service';
 import {UtilsService} from '../../../services/utils.service'
 import {BaseChart01Component} from '../base-chart01/base-chart01.component';
-import {SpeciesService} from '../../../services/species.service';
+import {TaxonomiesService} from '../../../services/taxonomies.service';
 import {StackedBars01Component} from '../stacked-bars01/stacked-bars01.component';
 import {SearchServiceParams, ChartDataFormat} from '../../../namespace';
 import { Filter} from '../../../components/search/namespace';
 
 @Component({
   selector: 'app-countries-chart03',
-  template: ` <app-bars01 [series]="series" *ngIf="series.length" xAxisTitle="Total number of species reported by countries" fontStyleItalic="true"></app-bars01> `,
+  template: ` <app-bars01 [series]="series" *ngIf="series.length" xAxisTitle="Total number of species reported by countries"></app-bars01> `,
   styles: []
 })
 export class CountriesChart03Component extends BaseChart01Component implements OnChanges {
     dataFormat=ChartDataFormat.keyval;
 
-    @Input() selectedTaxonomy:Filter;
-
-    constructor(_service:SpeciesService, _utilsService:UtilsService, _logger:LoggerService) {
+    constructor(_service:TaxonomiesService, _utilsService:UtilsService, _logger:LoggerService) {
         super(_service, _utilsService, _logger);
+        super.fetchData();
     }
 
     ngOnChanges(){
-        let params=new SearchServiceParams();
-        params.taxonomy=(this.selectedTaxonomy)?this.selectedTaxonomy.value:"";
-
-        super.fetchData(params);
     }
 
 }
