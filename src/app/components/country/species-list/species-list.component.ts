cc36d427cdd53f1e8be4327a21db3010d5ec9378
Fabrizio Antonangeli <fabrizio.antonangeli@gmail.com>
2020-06-18 11:13:54 +0200

    Removed unused function

--- src/app/components/country/species-list/species-list.component.ts
+++ src/app/components/country/species-list/species-list.component.ts
@@ -20,45 +20,6 @@ export class SpeciesListComponent implements OnChanges {
     constructor(private _service: CountryGroupsSpeciesService, private _utilsService:UtilsService, private _logger:LoggerService){
     }
 
-    /**
-     * load data for the table and set it to fishTableData
-     *
-     * @param {Object[]} data the data from the service
-     */
-    loadTableData(data){
-        let newdata;
-
-        if(!data || !data.Continents) return;
-
-        newdata=JSON.parse(JSON.stringify(data.Continents));
-
-        return newdata.sort((a, b) => (a.Name > b.Name) ? 1 : -1).map(e=>[
-            e.Name,
-            Number(e.Timeseries["2017"]).toLocaleString('en-US'),
-            Number(e.Species).toLocaleString('en-US'),
-            Number(e.FTypes).toLocaleString('en-US'),
-            Number(e.SFTypes).toLocaleString('en-US'),
-            e.Regions=e.Regions.sort((a, b) => (a.Name > b.Name) ? 1 : -1).map(r=>[
-                r.Name,
-                Number(r.Timeseries["2017"]).toLocaleString('en-US'),
-                Number(r.Species).toLocaleString('en-US'),
-                Number(r.FTypes).toLocaleString('en-US'),
-                Number(r.SFTypes).toLocaleString('en-US'),
-                r.Countries=r.Countries.sort((a, b) => (a.Name > b.Name) ? 1 : -1).map(c=>{
-                    let rv=[
-                        c.Name,
-                        Number(c.Timeseries["2017"]).toLocaleString('en-US'),
-                        Number(c.Species).toLocaleString('en-US'),
-                        Number(c.FTypes).toLocaleString('en-US'),
-                        Number(c.SFTypes).toLocaleString('en-US'),
-                    ];
-                    rv["Ccode"]=c.Ccode;
-                    return rv;
-                })
-
-            ])
-        ]);
-    }
 
     /**
      * fetch the sftype and load them 
