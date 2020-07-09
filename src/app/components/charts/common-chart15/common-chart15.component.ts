256861adf26f1faecbc04c2db1241a03ecd6fa1d
Fabrizio Antonangeli <fabrizio.antonangeli@gmail.com>
2020-07-09 15:44:53 +0200

    " Proportion of educational institutions offering training at different levels ":change title, add note, change to donut with total

--- src/app/components/charts/common-chart15/common-chart15.component.ts
+++ src/app/components/charts/common-chart15/common-chart15.component.ts
@@ -2,19 +2,19 @@ import { Component, Input, OnChanges } from '@angular/core';
 import {LoggerService} from '../../../services/logger.service';
 import {UtilsService} from '../../../services/utils.service'
 import {BaseChart01Component} from '../base-chart01/base-chart01.component';
-import {Pie01Component} from '../pie01/pie01.component';
 import {CommonChart15Service} from '../../../services/common/common-chart15.service';
 import {ChartDataFormat} from '../../../namespace';
 
 @Component({
   selector: 'app-common-chart15',
   template: `
-    <app-pie01 [series]="series" [legendEnabled]="false" [enableDataLabels]="true" *ngIf="series.length" [height]="300"></app-pie01>
+    <app-donut01 [series]="series" totalTemplate="Total number of<br>institutions<br>{total}" unit="institutions" [legendEnabled]="false" [enableDataLabels]="true" *ngIf="series.length" [height]="300"></app-donut01>
   `,
   styles: []
 })
 export class CommonChart15Component extends BaseChart01Component implements OnChanges {
     dataFormat=ChartDataFormat.keyval;
+    calcTotals=true;
 
     constructor(_service:CommonChart15Service, _utilsService:UtilsService, _logger:LoggerService) {
         super(_service, _utilsService, _logger);
