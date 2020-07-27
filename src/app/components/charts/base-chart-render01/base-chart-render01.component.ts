f865ed1a264b1389373f5579e41f3d744afde4bf
Fabrizio Antonangeli <fabrizio.antonangeli@gmail.com>
2020-07-27 12:25:37 +0200

    format this legend with "Risk", "No risk", "Potential risk"

--- src/app/components/charts/base-chart-render01/base-chart-render01.component.ts
+++ src/app/components/charts/base-chart-render01/base-chart-render01.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input, AfterViewInit, OnChanges } from '@angular/core';
-import * as Highcharts from 'highcharts';
-import Exporting from 'highcharts/modules/exporting';
+import { Component, Input, AfterViewInit, OnChanges } from "@angular/core";
+import * as Highcharts from "highcharts";
+import Exporting from "highcharts/modules/exporting";
 
 declare var require: any;
-let Boost = require('highcharts/modules/boost');
-let noData = require('highcharts/modules/no-data-to-display');
-let More = require('highcharts/highcharts-more');
+const Boost = require("highcharts/modules/boost");
+const noData = require("highcharts/modules/no-data-to-display");
+const More = require("highcharts/highcharts-more");
 
 Boost(Highcharts);
 noData(Highcharts);
@@ -13,125 +13,131 @@ More(Highcharts);
 noData(Highcharts);
 Exporting(Highcharts);
 
-export class BaseChartRender01Component  implements AfterViewInit, OnChanges {
-    wrapperId=`chart${Math.floor(Math.random() * 1000)}Container`;
-    options:any;
+export class BaseChartRender01Component implements AfterViewInit, OnChanges {
+  wrapperId = `chart${Math.floor(Math.random() * 1000)}Container`;
+  options: any;
 
-    @Input() series :object[]=[];
-    @Input() height:number=null;
-    @Input() fontStyleItalic:boolean=false;
-    @Input() xAxisTitle :string;
-    @Input() yAxisTitle :string;
+  @Input() series: object[] = [];
+  @Input() height: number = null;
+  @Input() fontStyleItalic: boolean = false;
+  @Input() xAxisTitle: string;
+  @Input() yAxisTitle: string;
 
+  /**
+   * optional custom tooltip point formatter
+   * @type {() => string}
+   */
+  @Input() pointFormatter: () => string;
 
-     /**
-      * if true the children of the nodes will be shown in the tooltip as a list
-      * @type {boolean}
-      */
-    @Input() showChildrenInTooltip:boolean=false;
+  /**
+   * if true the children of the nodes will be shown in the tooltip as a list
+   * @type {boolean}
+   */
+  @Input() showChildrenInTooltip: boolean = false;
 
-     /**
-      * title to show in the exportation (img/pdf)
-      * @type {string}
-      */
-    @Input() exportTitle: string="";
+  /**
+   * title to show in the exportation (img/pdf)
+   * @type {string}
+   */
+  @Input() exportTitle: string = "";
 
-    private _viewInitialized=false;
+  private _viewInitialized = false;
 
-    /**
-     * https://api.highcharts.com/highcharts/legend.enabled
-     */
-    @Input() legendEnabled :boolean=true;
+  /**
+   * https://api.highcharts.com/highcharts/legend.enabled
+   */
+  @Input() legendEnabled: boolean = true;
 
-     /**
-      * Unit to show in the tooltip
-      * @type {string}
-      */
-    @Input() unit:string="";
+  /**
+   * Unit to show in the tooltip
+   * @type {string}
+   */
+  @Input() unit: string = "";
 
-    /**
-     * show the chart
-     *
-     */
-    showChart(){
-        let options: any = {
-            title: {
-                text: null
-            },
-            xAxis: {
-            },
-            yAxis: {
-            },
-            exporting: {
-                chartOptions:{
-                    title: {
-                        text: this.exportTitle
-                    },
-                    subtitle:{
-                        y:30 //this position better the subtitle in donut01 
-                    }
-                },
-                buttons: {
-                    contextButton: {
-                        menuItems: ['downloadPDF', 'downloadPNG']
-                    }
-                }
-            },
-            legend: {
-                enabled: false
-            },
-            credits: {
-                enabled: false
-            },
-            tooltip: {},
-            ...this.options,
-        };
+  /**
+   * show the chart
+   *
+   */
+  showChart() {
+    let options: any = {
+      title: {
+        text: null,
+      },
+      xAxis: {},
+      yAxis: {},
+      exporting: {
+        chartOptions: {
+          title: {
+            text: this.exportTitle,
+          },
+          subtitle: {
+            y: 30, //this position better the subtitle in donut01
+          },
+        },
+        buttons: {
+          contextButton: {
+            menuItems: ["downloadPDF", "downloadPNG"],
+          },
+        },
+      },
+      legend: {
+        enabled: false,
+      },
+      credits: {
+        enabled: false,
+      },
+      tooltip: {},
+      ...this.options,
+    };
 
-        options.yAxis.labels=(options.yAxis.labels || {});
-        options.yAxis.labels.style=(options.yAxis.labels.style || {});
-        options.xAxis.labels=(options.xAxis.labels || {});
-        options.xAxis.labels.style=(options.xAxis.labels.style || {});
-        options.yAxis.labels=options.xAxis.labels.style={
-            fontStyle:(this.fontStyleItalic)?"italic":""
-        };
-        if(this.showChildrenInTooltip) {
-            options.tooltip.pointFormatter=this.pointWithChildrenFormatter;
-            options.tooltip.useHTML=true;
-        }
-
-        Highcharts.chart(this.wrapperId, options);
+    options.yAxis.labels = options.yAxis.labels || {};
+    options.yAxis.labels.style = options.yAxis.labels.style || {};
+    options.xAxis.labels = options.xAxis.labels || {};
+    options.xAxis.labels.style = options.xAxis.labels.style || {};
+    options.yAxis.labels = options.xAxis.labels.style = {
+      fontStyle: this.fontStyleItalic ? "italic" : "",
+    };
+    if (this.pointFormatter) {
+      options.tooltip.pointFormatter = this.pointFormatter;
+    }else if (this.showChildrenInTooltip) {
+      options.tooltip.pointFormatter = this.pointWithChildrenFormatter;
+      options.tooltip.useHTML = true;
     }
 
-    /**
-     * pointFormatter fn to used to show a point with children inside
-     *
-     * @returns {string} the html to show
-     */
-    pointWithChildrenFormatter=function(){
-                let shortData=(this.data || []), ttext="";
-                let fullDataLength=shortData.length;
-        
-                if(fullDataLength>10) shortData=this.data.slice(0,10);
+    Highcharts.chart(this.wrapperId, options);
+  }
 
-                ttext= `<b>${this.name}: ${this.y}</b><br />`+
-                shortData.map(function(e){
-                    return `<br /><span class='bullet'>•</span> ${e.key}: ${e.value}`;
-                });
-        
-                if(fullDataLength>10) ttext+="<br/>...";
+  /**
+   * pointFormatter fn to used to show a point with children inside
+   *
+   * @returns {string} the html to show
+   */
+  pointWithChildrenFormatter = function () {
+    let shortData = this.data || [],
+      ttext = "";
+    let fullDataLength = shortData.length;
 
-                return ttext;
+    if (fullDataLength > 10) shortData = this.data.slice(0, 10);
 
-    }
+    ttext =
+      `<b>${this.name}: ${this.y}</b><br />` +
+      shortData.map(function (e) {
+        return `<br /><span class='bullet'>•</span> ${e.key}: ${e.value}`;
+      });
 
-    ngAfterViewInit(){
-        this._viewInitialized=true;
-        this.showChart();
-    }
+    if (fullDataLength > 10) ttext += "<br/>...";
 
-    ngOnChanges(){
-        if(!this._viewInitialized) return;
+    return ttext;
+  };
 
-        this.showChart();
-    }
+  ngAfterViewInit() {
+    this._viewInitialized = true;
+    this.showChart();
+  }
+
+  ngOnChanges() {
+    if (!this._viewInitialized) return;
+
+    this.showChart();
+  }
 }
--- src/app/components/charts/common-chart09/common-chart09.component.ts
+++ src/app/components/charts/common-chart09/common-chart09.component.ts
@@ -1,26 +1,56 @@
-import { Component, Input, OnChanges } from '@angular/core';
-import {LoggerService} from '../../../services/logger.service';
-import {UtilsService} from '../../../services/utils.service'
-import {BaseChart01Component} from '../base-chart01/base-chart01.component';
-import {StackedColumns01Component} from '../stacked-columns01/stacked-columns01.component';
-import {CommonChart09Service} from '../../../services/common/common-chart09.service';
-import {ChartDataFormat} from '../../../namespace';
+import { Component, Input, OnChanges } from "@angular/core";
+import { LoggerService } from "../../../services/logger.service";
+import { UtilsService } from "../../../services/utils.service";
+import { BaseChart01Component } from "../base-chart01/base-chart01.component";
+import { StackedColumns01Component } from "../stacked-columns01/stacked-columns01.component";
+import { CommonChart09Service } from "../../../services/common/common-chart09.service";
+import { ChartDataFormat } from "../../../namespace";
 
 @Component({
-  selector: 'app-common-chart09',
+  selector: "app-common-chart09",
   template: `
-    <app-stacked-columns01 [series]="series" yAxisTitle="Number of species recorded" [legendEnabled]="false" *ngIf="series.length" [height]="300" [exportTitle]="exportTitle"></app-stacked-columns01>
+    <app-stacked-columns01
+      [series]="series"
+      yAxisTitle="Number of species recorded"
+      [pointFormatter]="pointFormatter"
+      [legendEnabled]="false"
+      *ngIf="series.length"
+      [height]="300"
+      [exportTitle]="exportTitle"
+    ></app-stacked-columns01>
   `,
-  styles: []
+  styles: [],
 })
 export class CommonChart09Component extends BaseChart01Component implements OnChanges {
-    dataFormat=ChartDataFormat.stackedKeyval;
+    unit = "specie";
+    dataFormat = ChartDataFormat.stackedKeyval;
 
-    constructor(_service:CommonChart09Service, _utilsService:UtilsService, _logger:LoggerService) {
+    constructor(
+        _service: CommonChart09Service,
+        _utilsService: UtilsService,
+        _logger: LoggerService
+    ) {
         super(_service, _utilsService, _logger);
     }
 
-    ngOnChanges(){
+    pointFormatter = function() {
+        let seriesName = "";
+        const unit = "species";
+
+        if (this.series.name === "No risk") {
+            seriesName = "No risk - no risk have been identified for this species";
+        } else if (this.series.name === "Potential risk") {
+            seriesName =
+                "Potential risk - risks have been identified but no impacts recorded";
+        } else if (this.series.name === "Risk") {
+            seriesName = "Risk - there are recorded impacts of this species";
+        }
+
+
+        return `<span style='color:${this.color}'>●</span> ${seriesName}: <b>${this.y} ${unit} (${this.percentage.toFixed(1)}%)`;
+    };
+
+    ngOnChanges() {
         super.ngOnChanges();
     }
 }
